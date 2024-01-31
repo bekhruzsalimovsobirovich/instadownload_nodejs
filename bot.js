f534f8ef6281483e55bb616693813ccab2dad458
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 <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>
2024-01-31 18:28:08 +0500

    updated

--- bot.js
+++ bot.js
@@ -12,6 +12,7 @@ bot.on("message", async (ctx) => {
     const link = ctx.update.message.text;
     llink = link.split('');
     if (llink[0] === "h", llink[1] === "t", llink[2] === "t", llink[3] === "p", llink[4] === "s", llink[5] === ":", llink[6] === "/", llink[7] === "/", llink[8] === "w", llink[9] === "w", llink[10] === "w", llink[11] === ".", llink[12] === "i", llink[13] === "n", llink[14] === "s", llink[15] === "t", llink[16] === "a", llink[17] === "g", llink[18] === "r", llink[19] === "a", llink[20] === "m") {
+        const loadingMessage = ctx.reply('🚀');
         const options = {
             method: 'GET',
             url: 'https://instagram-media-downloader.p.rapidapi.com/rapid/post.php',
@@ -26,6 +27,7 @@ bot.on("message", async (ctx) => {
         };
 
         request(options, async function (error, response, body) {
+            // console.log(response);
             if (error) throw new Error(error);
             const data = JSON.parse(body);
             await ctx.telegram.sendVideo(ctx.chat.id, {url: `${data.video}`}, {caption: 'Shakhzod Izatullaev'})
